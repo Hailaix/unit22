555baf1f235065e300b458986211ab5a2e5a5303
NHower <howernickoli@gmail.com>
2022-08-25 20:20:58 -0400

    outer space schema modified

--- sql-ddl-design/outer_space.sql
+++ sql-ddl-design/outer_space.sql
@@ -7,22 +7,41 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
-CREATE TABLE planets
-(
+CREATE TABLE galaxies(
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  name TEXT NOT NULL
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+CREATE TABLE stars(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  galaxy INTEGER REFERENCES galaxies ON DELETE CASCADE
+);
+
+CREATE TABLE planets(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  moons TEXT[],
+  orbital_period_in_years FLOAT NOT NULL,
+  orbits_around INTEGER REFERENCES stars ON DELETE CASCADE
+);
+
+-- CREATE TABLE planets
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   orbital_period_in_years FLOAT NOT NULL,
+--   orbits_around TEXT NOT NULL,
+--   galaxy TEXT NOT NULL,
+--   moons TEXT[]
+-- );
+
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
